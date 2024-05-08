3429f3c5e107a94b1991befc09d4094331178685
Anthony C. Jones <randomgeekdom@gmail.com>
2024-05-08 15:50:02 -0500

    Add project files.

---
+++ Myleau/ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Myleau.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ Myleau/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Myleau.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
